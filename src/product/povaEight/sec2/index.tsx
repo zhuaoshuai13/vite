9469a3f39af57d0e7d4d38477afac0d2760fac6a
zhuaoshuai13 <531024850@qq.com>
2023-09-27 11:27:22 +0800

    feated: 样式

--- src/product/povaEight/sec2/index.tsx
+++ src/product/povaEight/sec2/index.tsx
@@ -1,12 +1,7 @@
 import "./index.scss"
 
 const Sec2 = () => {
-  const global = window as any
-
-  return (
-    <section className='pop8 sec2'>
-    </section>
-  )
+  return <section className='pop8 sec2'></section>
 }
 
 export default Sec2
